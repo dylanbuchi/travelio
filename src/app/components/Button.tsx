7ca7a25b2122fb726c0bb15a7f702094896476f8
Dylan Buchi <devybuchi@gmail.com>
2023-04-02 07:25:00 -0300

    feat(Button): create Button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import clsx from "clsx";
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string;
+  isDisabled?: boolean;
+  isSmall?: boolean;
+  outline?: boolean;
+  icon?: IconType;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const buttonClasses = {
+  base: "relative w-full rounded-lg border-[2px] transition hover:opacity-80 disabled:cursor-not-allowed disabled:opacity-70",
+  outline: {
+    true: "bg-white border-black text-black hover:bg-gray-100",
+    false: "bg-teal-600 border-teal-700 text-white",
+  },
+  size: {
+    small: "py-1 text-sm font-light",
+    large: "py-3 text-md font-semibold",
+  },
+};
+
+const Button = ({
+  label,
+  isDisabled = false,
+  isSmall = false,
+  outline = false,
+  icon: Icon,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={isDisabled}
+      className={clsx(
+        buttonClasses.base,
+        buttonClasses.outline[outline ? "true" : "false"],
+        buttonClasses.size[isSmall ? "small" : "large"]
+      )}
+    >
+      {Icon && <Icon className="absolute left-4 top-3" size={24} />}
+      {label}
+    </button>
+  );
+};
+
+export default Button;
