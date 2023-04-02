1000f25d1ae4d19db519346fa5224047f1425618
Dylan Buchi <devybuchi@gmail.com>
2023-04-02 12:11:26 -0300

    chore(Layout): add ToasterProvider and RegisterModal components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "./styles/globals.css";
 import { Inter } from "next/font/google";
 import { Navbar } from "./components/navbar/Navbar";
 import { APP_NAME } from "./constants/app.constants";
+import RegisterModal from "./components/modals/RegisterModal";
+import { ToasterProvider } from "./providers/ToasterProvider";
 
 export const metadata = {
   title: APP_NAME,
@@ -19,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={fontFamily.className}>
+        <ToasterProvider />
         <Navbar />
+        <RegisterModal />
         {children}
       </body>
     </html>
