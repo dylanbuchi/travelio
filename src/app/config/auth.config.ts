f3c621b5a1d4c3849bf30fc00a70011232d1ca27
Dylan Buchi <devybuchi@gmail.com>
2023-04-03 12:58:59 -0300

    feat(config): add auth.config.ts with GitHub and Google providers

---
+++ src/app/config/auth.config.ts
@@ -0,0 +1,13 @@
+export const AUTH_CONFIG = {
+  githubProvider: {
+    clientId: process.env.GITHUB_AUTH_CLIENT_ID as string,
+    clientSecret: process.env.GITHUB_AUTH_CLIENT_SECRET as string,
+  },
+  googleProvider: {
+    clientId: process.env.GOOGLE_AUTH_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET as string,
+  },
+  nextAuth: {
+    secret: process.env.NEXT_AUTH_SECRET as string,
+  },
+};
