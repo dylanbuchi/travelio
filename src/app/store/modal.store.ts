18ba4c14416effcfa16b1486d2ae256cfddf2ec2
Dylan Buchi <devybuchi@gmail.com>
2023-04-05 17:44:33 -0300

    feat(store): add modal store zustand states

---
+++ src/app/store/modal.store.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+
+interface ModalStore {
+  modalName: string;
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+export const modalStore = (name: string) =>
+  create<ModalStore>((set) => ({
+    modalName: name,
+    isOpen: false,
+    openModal: () =>
+      set({
+        isOpen: true,
+      }),
+    closeModal: () =>
+      set({
+        isOpen: false,
+      }),
+  }));
+
+export const loginModalStore = modalStore("login");
+export const registerModalStore = modalStore("register");
