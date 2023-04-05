03925ed69dc7dd1147e24a9e24a4dc3b3e87b066
Dylan Buchi <devybuchi@gmail.com>
2023-04-05 14:57:52 -0300

    feat(buttons): add SOCIAL_BUTTONS constant

---
+++ src/app/components/buttons/constants.ts
@@ -0,0 +1,23 @@
+import { signIn } from "next-auth/react";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+
+export const SOCIAL_BUTTONS = [
+  {
+    id: 1,
+    icon: FcGoogle,
+    label: "Continue with Google",
+    onClick: () => {
+      signIn("google");
+    },
+  },
+
+  {
+    id: 2,
+    icon: AiFillGithub,
+    label: "Continue with GitHub",
+    onClick: () => {
+      signIn("github");
+    },
+  },
+];
