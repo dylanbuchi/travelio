4236729fb67d557539bd4e910926a5903c751358
Dylan Buchi <devybuchi@gmail.com>
2023-04-05 15:06:32 -0300

    fix(dto): set correct type and field name

--- src/app/dto/user.dto.ts
+++ src/app/dto/user.dto.ts
@@ -1,10 +1,12 @@
+import { User } from "@prisma/client";
+
 export interface UserDTO {
   id?: string;
   name: string;
   email: string;
 }
 
-export function convertUserToDTO(user: Partial<UserDTO>): UserDTO {
+export function convertUserToDTO(user: Partial<User>): UserDTO {
   return {
     id: user.email?.trim() || "",
     email: user.email?.trim() || "",
