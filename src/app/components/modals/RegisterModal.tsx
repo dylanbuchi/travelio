f23cc4d313cf7f2501d47a3b572202ccce4de9ac
Dylan Buchi <devybuchi@gmail.com>
2023-04-05 15:03:04 -0300

    refactor(RegisterModal): add SocialButtons component

--- src/app/components/modals/RegisterModal.tsx
+++ src/app/components/modals/RegisterModal.tsx
@@ -2,8 +2,6 @@
 
 import axios from "axios";
 import { useState } from "react";
-import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
 
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useRegisterModal } from "../../hooks/useRegisterModal";
@@ -12,7 +10,7 @@ import { Header } from "../Header";
 import { APP_NAME } from "@/app/constants/app.constants";
 import { Input } from "../inputs/Input";
 import { toast } from "react-hot-toast";
-import { Button } from "../Button";
+import { SocialButtons } from "../buttons/SocialButtons";
 
 export const RegisterModal = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -74,22 +72,7 @@ export const RegisterModal = () => {
   const Footer = () => (
     <>
       <span className="text-center">or</span>
-      <div className="mt-1 flex flex-col gap-4 text-neutral-500">
-        <Button
-          outline
-          icon={FcGoogle}
-          label="Continue with Google"
-          onClick={() => {}}
-        />
-      </div>
-      <div className="mt-1 flex flex-col gap-4 text-neutral-500">
-        <Button
-          outline
-          icon={AiFillGithub}
-          label="Continue with GitHub"
-          onClick={() => {}}
-        />
-      </div>
+      <SocialButtons />
       <div className="mt-2 flex items-center justify-center text-xs text-neutral-500">
         <footer>
           {"Already have an account? "}
