81a9fe2644a45956620a38f9f54c09cccdf4fec6
Dylan Buchi <devybuchi@gmail.com>
2023-04-07 09:26:28 -0300

    feat(categories): add CATEGORIES list constant

---
+++ src/app/components/navbar/categories/constants.ts
@@ -0,0 +1,103 @@
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCampingTent,
+  GiCastle,
+  GiCaveEntrance,
+  GiDesert,
+  GiWindmill,
+} from "react-icons/gi";
+
+import { MdOutlineVilla } from "react-icons/md";
+import { FaSkiing } from "react-icons/fa";
+import { BsSnow } from "react-icons/bs";
+import { IoDiamond } from "react-icons/io5";
+
+export const CATEGORIES = [
+  {
+    id: "beach",
+    label: "Beach",
+    icon: TbBeach,
+    description: "This property is closed to the beach",
+  },
+  {
+    id: "windmills",
+    label: "Windmills",
+    icon: GiWindmill,
+    description: "This property has windmills",
+  },
+  {
+    id: "modern",
+    label: "Modern",
+    icon: MdOutlineVilla,
+    description: "This is a modern property",
+  },
+  {
+    id: "country-side",
+    label: "Country Side",
+    icon: TbMountain,
+    description: "This property is in the country side",
+  },
+  {
+    id: "pools",
+    label: "Pools",
+    icon: TbPool,
+    description: "This property has a pool",
+  },
+  {
+    id: "lake",
+    label: "Lake",
+    icon: GiBoatFishing,
+    description: "This property is near a lake",
+  },
+  {
+    id: "skiing",
+    label: "Skiing",
+    icon: FaSkiing,
+    description: "This property is near skiing activities",
+  },
+  {
+    id: "castle",
+    label: "Castle",
+    icon: GiCastle,
+    description: "This property is near a castle",
+  },
+  {
+    id: "camping",
+    label: "Camping",
+    icon: GiCampingTent,
+    description: "This property is near camping activities",
+  },
+  {
+    id: "arctic",
+    label: "Arctic",
+    icon: BsSnow,
+    description: "This property is near camping activities",
+  },
+  {
+    id: "caves",
+    label: "Caves",
+    icon: GiCaveEntrance,
+    description: "This property is near caves",
+  },
+  {
+    id: "desert",
+    label: "Desert",
+    icon: GiDesert,
+    description: "This property is near deserts",
+  },
+  {
+    id: "barns",
+    label: "Barns",
+    icon: GiBarn,
+    description: "This property is in a barn",
+  },
+
+  {
+    id: "lux",
+    label: "Lux",
+    icon: IoDiamond,
+    description: "This is a luxurious property",
+  },
+];
