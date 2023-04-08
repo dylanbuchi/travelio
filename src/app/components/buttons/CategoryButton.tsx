4265dca01a610929c311334a3a4665dc017971aa
Dylan Buchi <devybuchi@gmail.com>
2023-04-08 13:56:14 -0300

    feat(buttons): create CategoryButton component

---
+++ src/app/components/buttons/CategoryButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import clsx from "clsx";
+import { CategoryItemProps } from "../navbar/categories/CategoryItem";
+
+interface CategoryButtonProps extends CategoryItemProps {
+  onClick: (category: string) => void;
+}
+
+const CategoryButton = ({
+  id,
+  label,
+  onClick,
+  icon: Icon,
+  isSelected,
+}: CategoryButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={() => onClick(id)}
+      className={clsx(
+        "flex w-full items-center justify-center gap-2 whitespace-nowrap rounded-xl border-2 p-4 text-xs transition hover:border-orange-600",
+        isSelected ? "border-orange-600" : "border-neutral-200",
+        isSelected ? "cursor-default" : "cursor-pointer"
+      )}
+    >
+      {label}
+      <Icon size={20} />
+    </button>
+  );
+};
+
+export default CategoryButton;
