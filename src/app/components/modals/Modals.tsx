2322db993b6eeba8efb2f47916e8331beae570fe
Dylan Buchi <devybuchi@gmail.com>
2023-04-08 13:28:52 -0300

    feat(Modals): create Modals component

---
+++ src/app/components/modals/Modals.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { RegisterModal } from "./RegisterModal";
+import { LoginModal } from "./LoginModal";
+import { RentModal } from "./RentModal";
+
+const Modals = () => {
+  return (
+    <>
+      <RegisterModal />
+      <LoginModal />
+      <RentModal />
+    </>
+  );
+};
+
+export default Modals;
