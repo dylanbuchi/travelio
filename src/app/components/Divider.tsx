27e7bc8f6ff708fe01510212cfd55b034b0fc086
Dylan Buchi <devybuchi@gmail.com>
2023-04-08 13:31:44 -0300

    feat(Divider): add smallText prop

--- src/app/components/Divider.tsx
+++ src/app/components/Divider.tsx
@@ -1,10 +1,20 @@
+import clsx from "clsx";
+
 interface DividerProps {
   text: string;
+  smallText: boolean;
 }
-export const Divider = ({ text }: DividerProps) => {
+export const Divider = ({ text, smallText }: DividerProps) => {
   return (
     <fieldset className="border-t border-slate-300">
-      <legend className="mx-auto px-4 text-base italic">{text}</legend>
+      <legend
+        className={clsx(
+          "mx-auto px-4 italic",
+          smallText ? "text-xs" : "text-base"
+        )}
+      >
+        {text}
+      </legend>
     </fieldset>
   );
 };
