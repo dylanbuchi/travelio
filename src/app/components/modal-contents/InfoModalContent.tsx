58725782a7318ff5f82811eed5973becabcf28fa
Dylan Buchi <devybuchi@gmail.com>
2023-04-09 13:07:44 -0300

    feat(modal-contents): create InfoModalContent component

---
+++ src/app/components/modal-contents/InfoModalContent.tsx
@@ -0,0 +1,27 @@
+import Counter, { CounterProps } from "@/app/components/Counter";
+import { ModalContentLayout } from "./ModalContentLayout";
+
+interface InfoModalContentProps {
+  counterData: CounterProps[];
+}
+
+export const InfoModalContent = ({ counterData }: InfoModalContentProps) => {
+  return (
+    <ModalContentLayout
+      title={"Share info about your place"}
+      subtitle={"What amenities do you have?"}
+    >
+      {counterData.map((item) => (
+        <>
+          <Counter
+            title={item.title}
+            subtitle={item.subtitle}
+            value={item.value}
+            onChange={item.onChange}
+          />
+          <hr />
+        </>
+      ))}
+    </ModalContentLayout>
+  );
+};
