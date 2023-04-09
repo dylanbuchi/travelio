4aa8409a3cfe6dc1c97beab166af6bcb260b9571
Dylan Buchi <devybuchi@gmail.com>
2023-04-09 13:49:26 -0300

    feat(hooks): create useCountries hook

---
+++ src/app/hooks/useCountries.tsx
@@ -0,0 +1,31 @@
+import countries from "world-countries";
+
+export interface Country {
+  label: string;
+  value: string;
+  flag: string;
+  region: string;
+  latlng: [number, number];
+}
+
+const formattedCountries: Country[] = countries.map((item) => ({
+  label: item.name.common,
+  value: item.cca2,
+  flag: item.flag,
+  region: item.region,
+  latlng: item.latlng,
+}));
+
+export const useCountries = () => {
+  function getCountries() {
+    return formattedCountries;
+  }
+
+  function getCountryByValue(value: string) {
+    return formattedCountries.find(
+      (item) => item.value.toLowerCase() === value.toLowerCase()
+    );
+  }
+
+  return { getCountries, getCountryByValue };
+};
