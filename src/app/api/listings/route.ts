70c28781c1669ca7de58f97d52f4bfe6512fbd0d
Dylan Buchi <devybuchi@gmail.com>
2023-04-15 14:44:52 -0300

    feat(api): add POST handler for creating a new listing with prisma

---
+++ src/app/api/listings/route.ts
@@ -0,0 +1,42 @@
+import { prismaClient } from "@/app/database/prisma-db";
+import { NextResponse } from "next/server";
+import { Listing } from "prisma/prisma-client";
+import { checkUserAuthentication } from "@/app/middleware/auth.middleware";
+
+export async function POST(request: Request) {
+  try {
+    const user = await checkUserAuthentication();
+    const listingData: Listing = await request.json();
+
+    const {
+      title,
+      description,
+      image,
+      category,
+      roomCount,
+      guestCount,
+      bathroomCount,
+      location,
+      price,
+    } = listingData;
+
+    const listing = await prismaClient.listing.create({
+      data: {
+        title,
+        description,
+        image,
+        category,
+        roomCount,
+        guestCount,
+        bathroomCount,
+        location,
+        price,
+        userId: user.id,
+      },
+    });
+
+    return NextResponse.json(listing);
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
