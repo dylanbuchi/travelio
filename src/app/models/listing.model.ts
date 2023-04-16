b638f78e0b549bc5dad784586616770ef4133a1c
Dylan Buchi <devybuchi@gmail.com>
2023-04-16 14:21:01 -0300

    feat(models): add ListingParams type

--- src/app/models/listing.model.ts
+++ src/app/models/listing.model.ts
@@ -1,5 +1,9 @@
 import { Listing } from "@prisma/client";
 
+export interface ListingParams {
+  listingId: string;
+}
+
 export type SerializedListing = Omit<Listing, "createdAt"> & {
   createdAt: string;
 };
