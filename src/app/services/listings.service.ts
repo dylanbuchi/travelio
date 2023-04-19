207f81e0480010e82ef71a740835a2c8c7fc4a3a
Dylan Buchi <devybuchi@gmail.com>
2023-04-19 12:09:40 -0300

    feat(services): add getListingById function

--- src/app/services/listings.service.ts
+++ src/app/services/listings.service.ts
@@ -1,4 +1,6 @@
-import { SerializedListing } from "../models/listing.model";
+import { ListingParams, SerializedListing } from "../models/listing.model";
+import { prismaClient } from "../database/prisma-db";
+import { serializeListing } from "../helpers/serializers.helper";
 
 export async function getListings() {
   try {
@@ -15,6 +17,24 @@ export async function getListings() {
     );
 
     return serializedListings;
+  } catch (error: any) {
+    throw new Error(error?.message);
+  }
+}
+
+export async function getListingById(params: ListingParams) {
+  try {
+    const listing = await prismaClient?.listing.findUnique({
+      where: {
+        id: params.listingId,
+      },
+    });
+
+    if (!listing) return;
+
+    const serializedListing = serializeListing(listing);
+
+    return serializedListing;
   } catch (error: any) {
     throw new Error(error?.message);
   }
