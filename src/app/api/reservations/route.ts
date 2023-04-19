685b285f40417f839ed6943e54c50ac0a221b464
Dylan Buchi <devybuchi@gmail.com>
2023-04-19 12:12:03 -0300

    feat(api): add POST handler for reservations

---
+++ src/app/api/reservations/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { prismaClient } from "@/app/database/prisma-db";
+import { checkUserAuthentication } from "@/app/middleware/auth.middleware";
+import { Reservation } from "@prisma/client";
+
+export async function POST(request: Request) {
+  try {
+    const user = await checkUserAuthentication();
+
+    const reservationData: Reservation = await request.json();
+
+    for (const key in reservationData) {
+      if (reservationData[key as keyof Reservation] == null) {
+        throw NextResponse.error();
+      }
+    }
+
+    const listingWithReservation = await prismaClient.listing.update({
+      where: {
+        id: reservationData.listingId,
+      },
+      data: {
+        reservations: {
+          create: {
+            createdAt: reservationData.createdAt,
+            endDate: reservationData.endDate,
+            startDate: reservationData.startDate,
+            totalPrice: reservationData.totalPrice,
+            userId: user.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(listingWithReservation);
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
