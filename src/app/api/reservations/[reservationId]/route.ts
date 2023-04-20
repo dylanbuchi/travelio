fe0d9abba8caac2f50db2251dab5e53033de4810
Dylan Buchi <devybuchi@gmail.com>
2023-04-20 16:30:04 -0300

    feat(api): add DELETE handler for reservations

---
+++ src/app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,25 @@
+import { prismaClient } from "@/app/database/prisma-db";
+import { checkUserAuthentication } from "@/app/middleware/auth.middleware";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { reservationId: string } }
+) {
+  const currentUser = await checkUserAuthentication();
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid reservation id");
+  }
+
+  const reservations = await prismaClient.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
+    },
+  });
+
+  return NextResponse.json(reservations);
+}
