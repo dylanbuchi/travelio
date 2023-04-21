6f333a56c4471d9e197033fc04657c598f7f0ea4
Dylan Buchi <devybuchi@gmail.com>
2023-04-21 13:28:26 -0300

    feat(services): add favorites service

---
+++ src/app/services/favorites.service.ts
@@ -0,0 +1,25 @@
+import { prismaClient } from "../database/prisma-db";
+import { serializeListing } from "../helpers/serializers.helper";
+import { checkUserAuthentication } from "../middleware/auth.middleware";
+
+export async function getFavoriteListings() {
+  try {
+    const user = await checkUserAuthentication();
+
+    const favoriteListings = await prismaClient.listing.findMany({
+      where: {
+        id: {
+          in: [...user.favoriteIds],
+        },
+      },
+    });
+
+    const serializedFavoriteListings = favoriteListings.map((listing) => {
+      return serializeListing(listing);
+    });
+
+    return serializedFavoriteListings;
+  } catch (error: any) {
+    throw new Error(error?.message);
+  }
+}
