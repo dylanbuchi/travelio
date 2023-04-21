4767239dce555429b160ce155cb63cdf48a7dc48
Dylan Buchi <devybuchi@gmail.com>
2023-04-21 17:26:42 -0300

    feat(models): add new props to ListingParams

--- src/app/models/listing.model.ts
+++ src/app/models/listing.model.ts
@@ -1,7 +1,18 @@
 import { Listing } from "@prisma/client";
 
 export interface ListingParams {
-  listingId: string;
+  listingId?: string;
+  userId?: string;
+
+  category?: string;
+
+  bathroomCount?: string;
+  roomCount?: string;
+  guestCount?: string;
+
+  startDate?: string;
+  endDate?: string;
+  location?: string;
 }
 
 export type SerializedListing = Omit<Listing, "createdAt"> & {
