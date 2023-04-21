33f0722ed8e46fc66c3c16fc0c263186b302a580
Dylan Buchi <devybuchi@gmail.com>
2023-04-21 17:37:11 -0300

    chore(app): add ListingCards and SearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return "Home";
+import { ListingCard } from "./components/listings/ListingCard";
+import { NoResults } from "./components/NoResults";
+import { ListingParams } from "./models/listing.model";
+import { getListings } from "./services/listings.service";
+import { getCurrentUser } from "./services/user.session";
+
+interface SearchParams {
+  searchParams: ListingParams;
+}
+
+export default async function Home({ searchParams }: SearchParams) {
+  const listings = await getListings(searchParams);
+  const user = await getCurrentUser();
+
+  if (!listings?.length) return <NoResults showResetButton />;
+
+  return (
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 gap-8 p-10 pt-[11.5rem] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {listings.map((listing) => (
+          <ListingCard user={user} listing={listing} key={listing.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
